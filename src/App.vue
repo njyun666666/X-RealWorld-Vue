f6d32f941fc5b8e003414fcc592af26c1d36bec7
Jyun <njyun666666@gmail.com>
2024-03-25 16:34:36 +0800

    fix

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
+import { useDark } from '@vueuse/core'
 import { useI18n } from 'vue-i18n'
 import { RouterView } from 'vue-router'
 
 const { locale } = useI18n()
 document.documentElement.setAttribute('lang', locale.value)
+useDark()
 </script>
 
 <template>
