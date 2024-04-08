6a365292360a786eb7ecc35f85f6a0564f36eecb
Jyun <njyun666666@gmail.com>
2024-04-08 22:19:14 +0800

    logout

--- src/layouts/XLayout.vue
+++ src/layouts/XLayout.vue
@@ -7,8 +7,15 @@ import { useLoginStore } from '@/stores/login'
 import { RouterLink } from 'vue-router'
 import Nav from './Nav.vue'
 import { breakpoints } from '@/libs/common'
+import Avatar from 'primevue/avatar'
+import OverlayPanel from 'primevue/overlaypanel'
+import { ref } from 'vue'
 
 const login = useLoginStore()
+const op = ref()
+const toggle = (event: MouseEvent) => {
+  op.value.toggle(event)
+}
 </script>
 <template>
   <div
@@ -40,33 +47,62 @@ const login = useLoginStore()
           <div
             class="sticky bottom-0 z-[1] flex w-full flex-col gap-2 bg-background pb-1 pl-1 pr-1 pt-2 md:pr-8"
           >
-            <RouterLink
-              :to="{ name: 'login' }"
-              v-if="!login.loginState"
-              v-tooltip="{
-                value: $t('action.Login'),
-                disabled: breakpoints.active().value == 'xl'
-              }"
-            >
-              <Button severity="secondary" rounded :class="cn('h-12 w-12 xl:w-full')">
-                <font-awesome-icon icon="fa-solid fa-right-to-bracket" class="!h-5 !w-5" />
-                <span class="ml-2 hidden xl:block">{{ $t('action.Login') }}</span>
-              </Button>
-            </RouterLink>
+            <template v-if="!login.loginState">
+              <RouterLink
+                :to="{ name: 'login' }"
+                v-tooltip="{
+                  value: $t('action.Login'),
+                  disabled: breakpoints.active().value == 'xl'
+                }"
+              >
+                <Button severity="secondary" rounded :class="cn('h-12 w-12 xl:w-full')">
+                  <font-awesome-icon icon="fa-solid fa-right-to-bracket" class="!h-5 !w-5" />
+                  <span class="ml-2 hidden xl:block">{{ $t('action.Login') }}</span>
+                </Button>
+              </RouterLink>
 
-            <RouterLink
-              :to="{ name: 'register' }"
-              v-if="!login.loginState"
-              v-tooltip="{
-                value: $t('action.Register'),
-                disabled: breakpoints.active().value == 'xl'
-              }"
-            >
-              <Button rounded :class="cn('h-12 w-12 xl:w-full')">
-                <font-awesome-icon icon="fa-solid fa-user-plus" class="!h-5 !w-5" />
-                <span class="ml-2 hidden xl:block">{{ $t('action.Register') }}</span>
+              <RouterLink
+                :to="{ name: 'register' }"
+                v-tooltip="{
+                  value: $t('action.Register'),
+                  disabled: breakpoints.active().value == 'xl'
+                }"
+              >
+                <Button rounded :class="cn('h-12 w-12 xl:w-full')">
+                  <font-awesome-icon icon="fa-solid fa-user-plus" class="!h-5 !w-5" />
+                  <span class="ml-2 hidden xl:block">{{ $t('action.Register') }}</span>
+                </Button>
+              </RouterLink>
+            </template>
+            <template v-else>
+              <Button rounded text plain :class="cn('h-12 w-12 !p-0 xl:w-full')" @click="toggle">
+                <div class="flex w-full items-center justify-center xl:justify-start xl:p-4">
+                  <Avatar
+                    v-if="login.user && login.user.image"
+                    :image="login.user.image"
+                    shape="circle"
+                    class="!h-8 !w-8 cursor-pointer"
+                  />
+                  <Avatar v-else shape="circle" class="!h-8 !w-8 cursor-pointer">
+                    <template #icon>
+                      <font-awesome-icon icon="fa-solid fa-user" />
+                    </template>
+                  </Avatar>
+                  <div class="ml-3 hidden xl:block">
+                    <div class="text-left text-sm font-bold">{{ login.user.username }}</div>
+                    <div class="text-left text-xs text-foreground/80">{{ login.user.email }}</div>
+                  </div>
+                </div>
               </Button>
-            </RouterLink>
+              <OverlayPanel ref="op">
+                <div class="flex flex-col">
+                  <Button rounded text plain @click="login.logout()">
+                    <font-awesome-icon icon="fa-solid fa-right-from-bracket" class="!h-5 !w-5" />
+                    <span class="ml-2">{{ $t('action.Logout') }}</span>
+                  </Button>
+                </div>
+              </OverlayPanel>
+            </template>
           </div>
         </div>
       </ScrollPanel>
