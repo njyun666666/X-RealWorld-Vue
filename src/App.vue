5e0f8e54f9857237f233d3b6de8505490a67a7c8
Jyun <njyun666666@gmail.com>
2024-04-29 23:01:38 +0800

    setting toast

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@ import { useI18n } from 'vue-i18n'
 import { RouterView } from 'vue-router'
 import { useLocaleStore } from './stores/locale'
 import type { LangType } from './i18n/config'
-import Toast from 'primevue/toast'
+import Toast from './components/UI/Toast.vue'
 
 const { locale } = useI18n()
 const localeStore = useLocaleStore()
@@ -15,5 +15,5 @@ useDark()
 
 <template>
   <RouterView />
-  <Toast position="bottom-center" />
+  <Toast />
 </template>
