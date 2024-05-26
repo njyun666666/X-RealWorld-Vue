13ac9ffa43dac4c230d74de6339ead9889f78e69
Jyun <njyun666666@gmail.com>
2024-05-26 20:47:08 +0800

    image loading

--- src/components/Profile/ProfileImage.vue
+++ src/components/Profile/ProfileImage.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import Image from 'primevue/image'
+import Image from '../UI/Image.vue'
 import Avatar from 'primevue/avatar'
 
 defineProps<{
@@ -8,6 +8,6 @@ defineProps<{
 }>()
 </script>
 <template>
-  <Image v-if="image" :src="image" alt="Image" preview />
+  <Image v-if="image" :src="image" preview width="100%" height="100%" />
   <Avatar v-else :label="username.substring(0, 1)" size="xlarge" shape="circle" />
 </template>
