9ddc2f680d1400afd1e921a0423240661dafb0d9
Jyun <njyun666666@gmail.com>
2024-05-29 23:09:13 +0800

    fix

--- src/components/Slots/ItemSlot.vue
+++ src/components/Slots/ItemSlot.vue
@@ -13,11 +13,11 @@ const props = defineProps<{
     <div class="flex w-full">
       <div class="flex flex-col pr-2">
         <div class="flex h-4 w-full shrink-0 justify-center pb-1">
-          <div v-if="connectLinePrev" class="h-full w-0.5 bg-border"></div>
+          <div v-if="connectLinePrev" class="h-full w-0.5 rounded-b-full bg-border"></div>
         </div>
         <slot name="ProfileImage"></slot>
         <div class="flex h-4 w-full grow justify-center pt-1">
-          <div v-if="connectLineNext" class="h-full w-0.5 bg-border"></div>
+          <div v-if="connectLineNext" class="h-full w-0.5 rounded-t-full bg-border"></div>
         </div>
       </div>
       <div class="relative grow pt-4">
