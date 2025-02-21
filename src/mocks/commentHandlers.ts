7bc3a141fbe96eeb48bcbca5dacea02d87596e46
Jyun <njyun666666@gmail.com>
2025-02-21 09:53:29 +0800

    Add delay to mock API handlers for improved response simulation

--- src/mocks/commentHandlers.ts
+++ src/mocks/commentHandlers.ts
@@ -3,14 +3,16 @@ import { HttpResponse, delay, http } from 'msw'
 import { multipleCommentFaker, singleCommentFaker } from '@/libs/faker/commentFaker'
 
 export const commentHandlers = [
-  http.post(`${appConfig.REALWORLD_API}/api/articles/:slug/comments`, ({ params }) => {
+  http.post(`${appConfig.REALWORLD_API}/api/articles/:slug/comments`, async ({ params }) => {
+    await delay()
     return HttpResponse.json(singleCommentFaker(params.slug as string))
   }),
   http.get(`${appConfig.REALWORLD_API}/api/articles/:slug/comments`, async ({ params }) => {
     await delay()
     return HttpResponse.json(multipleCommentFaker(params.slug as string))
   }),
-  http.delete(`${appConfig.REALWORLD_API}/api/articles/:slug/comments`, () => {
+  http.delete(`${appConfig.REALWORLD_API}/api/articles/:slug/comments`, async () => {
+    await delay()
     return HttpResponse.json({})
   })
 ]
